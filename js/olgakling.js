b386b639a4f59c73dc092a0407e557b8b2527b72
Sergey Poliakov <ps.force@gmail.com>
2016-11-30 14:28:27 +0200

    Mobile friendly

--- js/olgakling.js
+++ js/olgakling.js
@@ -46,7 +46,15 @@ $(document).ready(function() {
     $("#lightSlider7").lightSlider({
         item: 2,
         loop: true,
-        adaptiveHeight:true
+        adaptiveHeight:true,
+        responsive: [
+            {
+                breakpoint:800,
+                settings: {
+                    item:1
+                }
+            }
+        ]
     });
 
     //меню
